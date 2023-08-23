f8c4697810b198b873d1cc27b8b1aa7a0f788dfb
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-08-23 23:59:04 +0300

    just commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
     CarrierWave.configure do |config|
       config.storage    = :aws
       config.aws_bucket = ENV['S3_BUCKET_NAME']
-      config.aws_acl    = 'public-read'
+      config.aws_acl    = 'private'
       config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
   
       config.aws_credentials = {
