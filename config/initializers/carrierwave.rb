a3819b0315900c3ab305e834e4385230a4ddc14c
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-08-23 19:40:26 +0300

    use env var

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@
 if Rails.env.production?
     CarrierWave.configure do |config|
       config.storage    = :aws
-      config.aws_bucket = Rails.application.credentials.aws_s3[:S3_BUCKET_NAME]
+      config.aws_bucket = ENV['S3_BUCKET_NAME']
       config.aws_acl    = 'public-read'
       config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
   
       config.aws_credentials = {
-        access_key_id: Rails.application.credentials.aws_s3[:AWS_ACCESS_KEY_ID],
-        secret_access_key: Rails.application.credentials.aws_s3[:AWS_SECRET_ACCESS_KEY],
-        region: Rails.application.credentials.aws_s3[:AWS_REGION] # Required
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: ENV['AWS_REGION'] # Required
       }
     end
   else
