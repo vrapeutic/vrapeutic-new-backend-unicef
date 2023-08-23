3cc62a153350874b7f0193376e719a27851e3672
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-08-23 18:11:19 +0300

    return files data

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,4 +1,11 @@
 class DoctorSerializer
   include JSONAPI::Serializer
   attributes :name, :email, :degree, :university, :is_email_verified
+
+  attribute :photo_url do |doctor|
+    doctor.photo_url
+  end
+  attribute :certificate_url do |doctor|
+    doctor.certificate_url
+  end
 end
