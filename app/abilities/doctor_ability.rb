026b1b3ff1f87c3e27420e99001051e9a606bd44
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-09-11 22:40:50 +0300

    finish authorization

--- app/abilities/doctor_ability.rb
+++ app/abilities/doctor_ability.rb
@@ -7,6 +7,8 @@ class DoctorAbility
         can :update, Doctor if Authorization::Doctor::CanEditProfileService.new(current_doctor: doctor, updated_doctor_id: params[:id]).call
       when 'center_assigned_children'
         can :center_assigned_children, Doctor if Authorization::Doctor::CanGetAssignedCenterChildrenService.new(current_doctor: doctor, center_id: params[:center_id]).call
+      when 'center_headsets'
+        can :center_headsets, Doctor if Authorization::Doctor::CanGetCenterHeadsetsService.new(current_doctor: doctor, center_id: params[:center_id]).call
 
       else
         false
