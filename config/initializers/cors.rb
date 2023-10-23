6a902efa800d318a5d79ebd9675f5ed302b5d681
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-10-23 15:38:44 +0300

    solve cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,12 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+    allow do
+      origins [/http:\/\/*/, /https:\/\/*/]
+      resource '*',
+               headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+end
