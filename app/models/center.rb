1232ab1a8c312331f803e26102c9103327eb4261
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-11-09 15:07:31 +0200

    make  longitude and lititude optional

--- app/models/center.rb
+++ app/models/center.rb
@@ -1,7 +1,7 @@
 class Center < ApplicationRecord
 
-    validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
-    validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+    validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_nil: true
+    validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_nil: true
     validates :name, presence: true
     validates :website, uniqueness: true
     validates :logo, presence: true
