1d814de6c1b919c6bcde95c7c762f11178af4661
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-12-31 17:06:33 +0200

    seed data

---
+++ db/migrate/20231231150553_seed_assigned_center_module.rb
@@ -0,0 +1,8 @@
+class SeedAssignedCenterModule < ActiveRecord::Migration[7.0]
+  def change
+    result = CenterSoftwareModule.all 
+    result.each do |record|
+      AssignedCenterModule.create(center_id: record.center_id, software_module_id: record.software_module_id, end_date: Time.now + 1.years)
+    end
+  end
+end
