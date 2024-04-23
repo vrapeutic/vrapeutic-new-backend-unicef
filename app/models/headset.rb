949ba8aafae91676e2cdfcaf83640a6976261c49
Ahmed Abdelhamid <eng.a.abdelhamid@outlook.com>
2024-04-24 01:52:38 +0200

    feat: allo admin doctors to add headset to center (#1)

--- app/models/headset.rb
+++ app/models/headset.rb
@@ -1,18 +1,14 @@
 class Headset < ApplicationRecord
 
-  validates :name, presence: true
-  validates :model, presence: true
   validates :brand, presence: true
   validates :key, presence: true, uniqueness: true
 
   belongs_to :center
   has_many :sessions, dependent: :destroy
 
-  
-
   before_validation :lowercase_name
 
-  private 
+  private
 
   def lowercase_name
     self.name = self.name.downcase if self.name
