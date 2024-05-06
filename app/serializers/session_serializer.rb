ea0f745baf8f17ee7e31d4edc50a5af49e6f3e5c
Ahmed Abdelhamid <eng.a.abdelhamid@outlook.com>
2024-05-06 17:17:06 +0300

    feat: automate session_id for sessions

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,4 +1,7 @@
 class SessionSerializer
   include JSONAPI::Serializer
-  attributes :center_id, :headset_id, :child_id, :duration, :evaluation, :is_verified, :ended_at
+  attributes :center_id, :headset_id, :child_id, :duration, :evaluation, :is_verified, :ended_at, :session_id, :created_at, :updated_at
+
+  has_many :software_modules, if: proc { |_record, params| BaseSerializer.params_include?(params, 'software_modules') }
+  has_many :doctors, if: proc { |_record, params| BaseSerializer.params_include?(params, 'doctors') }
 end
