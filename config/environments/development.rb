f6154aac6e4245cf8856e2cccafbe57f756c259b
Ahmed Abdelhamid <eng.a.abdelhamid@outlook.com>
2024-05-13 00:16:56 +0300

    use letter opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,26 +62,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
-
-  config.action_mailer.default_options = {
-    from: ENV["ACTION_MAILER_EMAIL"]
-  }
-
-  config.action_mailer.smtp_settings = {
-    :address                => ENV["ACTION_MAILER_ADDRESS"],
-    :port                   => ENV["ACTION_MAILER_PORT"],
-    :domain                 => ENV["ACTION_MAILER_DOMAIN"],
-    :user_name              => ENV["ACTION_MAILER_EMAIL"],
-    :password               => ENV["ACTION_MAILER_PASSWORD"],
-    :authentication         => :login,
-    :enable_starttls_auto   => false,
-    :ssl                    => true,
-    :tls                    => true,
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
