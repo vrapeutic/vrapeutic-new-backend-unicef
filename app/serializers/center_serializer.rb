fa35894b25b799800285dcf6f3e27bac872c8131
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2024-08-08 22:05:13 +0300

    add regisration number for center serializer

--- app/serializers/center_serializer.rb
+++ app/serializers/center_serializer.rb
@@ -1,6 +1,6 @@
 class CenterSerializer < BaseSerializer
   attributes :name, :longitude, :latitude, :logo, :website, :email, :phone_number,
-             :tax_id, :certificate
+             :tax_id, :certificate, :registration_number
 
   attribute :logo_url do |center|
     center.logo_url
