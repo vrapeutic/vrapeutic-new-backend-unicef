ad5ec1f5706efcf93c5bfd3e091cdeef38efe7a9
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2024-10-02 02:46:53 +0300

    use ebextensions to run sidekiq

---
+++ .platform/hooks/postdeploy/50_restart_sidekiq.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# this file should be located at: .platform/hooks/postdeploy/50_restart_sidekiq.sh
+# mod: a@barot.us
+
+EB_APP_DEPLOY_DIR=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppDeployDir)
+EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppUser)
+RACK_ENV=$(/opt/elasticbeanstalk/bin/get-config environment -k RACK_ENV)
+SIDEKIQ_PID=$(pgrep -u "$EB_APP_USER" -f sidekiq)
+SIDEKIQ_LOG="$EB_APP_DEPLOY_DIR/log/sidekiq.log"
+
+cd "$EB_APP_DEPLOY_DIR"
+
+if [ -n "$SIDEKIQ_PID" ]; then
+  echo "terminating existing sidekiq"
+  kill -TERM "$SIDEKIQ_PID"
+  # wait process to get killed
+  while kill -0 "$SIDEKIQ_PID" 2>/dev/null; do
+    sleep 1
+  done
+fi
+
+set -xe
+export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+
+echo "starting sidekiq"
+su "$EB_APP_USER" -c "bundle exec sidekiq -e $RACK_ENV > $SIDEKIQ_LOG 2>&1 & disown"
