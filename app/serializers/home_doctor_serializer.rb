876f1579e533a3f047c1864ae34fe950099b4698
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2024-10-25 01:04:46 +0300

    feat: flag doctor with is_admin_center

--- app/serializers/home_doctor_serializer.rb
+++ app/serializers/home_doctor_serializer.rb
@@ -1,6 +1,12 @@
 class HomeDoctorSerializer < BaseSerializer
   attributes :name, :join_date, :number_of_sessions, :specialties, :photo_url, :degree
 
+  attribute :is_center_admin, if: proc { |_doctor, params|
+    params && params[:center_id]
+  } do |doctor, params|
+    doctor.doctor_centers.find_by(center_id: params[:center_id]).admin?
+  end
+
   has_many :specialties, if: proc { |_record, params| BaseSerializer.params_include?(params, 'specialties') }
   has_many :centers, if: proc { |_record, params| BaseSerializer.params_include?(params, 'centers') }
   has_many :children, if: proc { |_record, params| BaseSerializer.params_include?(params, 'children') }
