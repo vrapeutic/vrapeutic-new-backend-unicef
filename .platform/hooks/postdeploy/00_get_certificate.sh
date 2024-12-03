1c2949cb290f1b61580a2aef62b11669dbc596ec
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2024-12-03 03:52:41 +0200

    extract domain from configs

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-sed -i '/server_tokens off;/a server_names_hash_bucket_size 512;' /etc/nginx/nginx.conf
-sudo certbot -n -d vrapeutic-unicef.eu-west-1.elasticbeanstalk.com --nginx --agree-tos --email eng.a.abdelhamid@outlook.com
+DOMAIN=$(/opt/elasticbeanstalk/bin/get-config environment -k DOMAIN)
+
+if [ -n "$DOMAIN" ]; then
+	sed -i '/server_tokens off;/a server_names_hash_bucket_size 512;' /etc/nginx/nginx.conf
+	sudo certbot -n -d $DOMAIN --nginx --agree-tos --email eng.a.abdelhamid@outlook.com
+else
+    echo "DOMAIN does not exist or is empty."
+fi
