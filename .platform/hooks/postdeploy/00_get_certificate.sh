0df07e0b1ad4e50562ef17b888ad71c0faa11cfb
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2024-12-03 02:16:45 +0200

    generate ssl cert

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# sudo certbot -n -d vrapeutic-unicef.eu-west-1.elasticbeanstalk.com --nginx --agree-tos --email eng.a.abdelhamid@outlook.com
+# sudo certbot install --cert-name vrapeutic-unicef.eu-west-1.elasticbeanstalk.com
