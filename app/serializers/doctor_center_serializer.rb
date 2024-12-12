290f300714e3f244ca9f6e0421738016a8ea41a8
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2024-12-12 18:18:40 +0200

    return the invited by name

--- app/serializers/doctor_center_serializer.rb
+++ app/serializers/doctor_center_serializer.rb
@@ -5,8 +5,8 @@ class DoctorCenterSerializer < BaseSerializer
     doctor_center.approved? && doctor_center.doctor.sessions&.any? ? doctor_center.doctor.sessions.where(center_id: doctor_center.center_id).count : 0
   end
 
-  attribute :invited_by_email do |doctor_center|
-    doctor_center&.invited_by&.email
+  attribute :invited_by_name do |doctor_center|
+    doctor_center&.invited_by&.name
   end
 
   has_one :center, if: proc { |_record, params| BaseSerializer.params_include?(params, 'center') }
