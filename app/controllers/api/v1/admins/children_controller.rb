6eb18bf2fb69ffbfaa56fb7919c3a78f86815780
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2025-02-23 00:22:17 +0200

    feat: add center for diagnoses on edit child

--- app/controllers/api/v1/admins/children_controller.rb
+++ app/controllers/api/v1/admins/children_controller.rb
@@ -1,24 +1,25 @@
-class Api::V1::Admins::ChildrenController < Api::BaseApi
+class Api::V1::Admins::ChildrenController < Api::V1::BaseApi
   before_action :authorized_admin?
   before_action :set_child, only: %i[show update]
 
   def index
-    q = Child.ransack_query(sort: params[:sort], query: params[:q])
-    render json: ChildSerializer.new(q.result(distinct: true), param_options).serializable_hash
+    q = Child.ransack_query(sort: params[:sort], query: params[:q], default_sort: 'name asc')
+    render json: V1::ChildSerializer.new(q.result(distinct: true), param_options).serializable_hash
   end
 
   def show
-    render json: ChildSerializer.new(@child, param_options).serializable_hash
+    render json: V1::ChildSerializer.new(@child, param_options).serializable_hash
   end
 
   def update
     child = Admin::EditChildService.new(
       child_id: @child.id,
+      center_ids: @child.centers.pluck(:id),
       edit_params: child_params.except(:diagnosis_ids),
       diagnosis_ids: params[:child][:diagnosis_ids]
     ).call
 
-    render json: ChildSerializer.new(child, param_options).serializable_hash
+    render json: V1::ChildSerializer.new(child, param_options).serializable_hash
   rescue StandardError => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
